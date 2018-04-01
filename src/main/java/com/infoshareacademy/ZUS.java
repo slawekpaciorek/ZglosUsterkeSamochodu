f2b8f1c5bcc1a017928ea89340932cfe94ca5b76
Slawek Paciorek <slawekpaciorek@gmail.com>
2018-04-01 22:20:37 +0200

    JJDZW1SAM-Konfiguracja-srodowiska : Made few changes in main class

--- src/main/java/com/infoshareacademy/ZUS.java
+++ src/main/java/com/infoshareacademy/ZUS.java
@@ -1,13 +1,10 @@
 package com.infoshareacademy;
 
-/**
- * Hello world!
- *
- */
 public class ZUS
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        System.out.println( "Witam w aplikacji do zgłaszania usterek twojego samochodu." );
+
     }
 }
