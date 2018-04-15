2d09a8ea89b53903cf1fae606b32324708550a23
Slawek Paciorek <slawekpaciorek@gmail.com>
2018-04-15 20:16:07 +0200

    JJDZW1SAM-16 : Brands class

--- src/main/java/com/infoshareacademy/ZUS.java
+++ src/main/java/com/infoshareacademy/ZUS.java
@@ -1,10 +1,43 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.functions.JSONLoader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class ZUS
 {
+
+
     public static void main( String[] args ) {
+
+
+
 
         System.out.println( "Witam w aplikacji do zgłaszania usterek twojego samochodu." );
 
+
+        Brands brands = new Brands();
+        ArrayList brandsList = brands.getBrands();
+
+        JSONLoader jsonLoader = new JSONLoader();
+
+
+
+
+
+        try {
+            if(jsonLoader.showDataArray() != null){
+                for(int i = 0; i < jsonLoader.showDataArray().size(); i++)
+                {
+                    brandsList.add(jsonLoader.showDataArray().getJsonObject(i));
+                }
+            }
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println(brandsList);
     }
 }
