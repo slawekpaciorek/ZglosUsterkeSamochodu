1ffe9f637d4aea9f9c967bb22dd0e1e452bd0657
slawekpaciorek <33649657+slawekpaciorek@users.noreply.github.com>
2018-04-19 07:45:17 +0200

    Update ZUS.java

--- src/main/java/com/infoshareacademy/ZUS.java
+++ src/main/java/com/infoshareacademy/ZUS.java
@@ -7,9 +7,20 @@ public class ZUS
     private static ZusFactory instruction = ZusFactory.init();
 
     public static void main( String[] args ) {
+        
+        Scanner inputReader = new Scanner(System.in);
+        private boolean stop = false;
 
+        while(!stop){
+            
         String input = "menu";
-        instruction.follow(input);
+        String input = instruction.follow(input);
 
+        stop = input.equals("exit");
+        
+        System.out.println();
+        instruction.follow(input);
+        System.out.println();
+            
     }
 }
