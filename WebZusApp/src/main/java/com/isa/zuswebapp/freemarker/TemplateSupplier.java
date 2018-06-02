ff0280efe73457b424efdaf3043035b484a486e5
Slawek Paciorek <slawekpaciorek@gmail.com>
2018-06-03 01:22:18 +0200

    Add working servlets

--- WebZusApp/src/main/java/com/isa/zuswebapp/freemarker/TemplateSupplier.java
+++ WebZusApp/src/main/java/com/isa/zuswebapp/freemarker/TemplateSupplier.java
@@ -2,13 +2,16 @@ package com.isa.zuswebapp.freemarker;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
 
+import javax.ejb.Stateless;
 import javax.servlet.ServletContext;
 import java.io.IOException;
 
+@Stateless
 public class TemplateSupplier {
 
-    public static final String TEMPLATES_DIRECTORY_PATHS = "/freemarker-templates";
+    public static final String TEMPLATES_DIRECTORY_PATHS = "freemarker-templates";
 
     public static Template createTemplate(ServletContext servletContext, String templateName) throws IOException{
 
@@ -16,5 +19,12 @@ public class TemplateSupplier {
 
         config.setServletContextForTemplateLoading(servletContext, TEMPLATES_DIRECTORY_PATHS);
         config.setDefaultEncoding("UTF-8");
+        config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        config.setLogTemplateExceptions(false);
+        config.setWrapUncheckedExceptions(true);
+
+        Template temp = config.getTemplate(templateName);
+
+        return temp;
     }
 }
--- WebZusApp/src/main/java/com/isa/zuswebapp/servlets/IndexServlet.java
+++ WebZusApp/src/main/java/com/isa/zuswebapp/servlets/IndexServlet.java
@@ -1,14 +1,25 @@
 package com.isa.zuswebapp.servlets;
 
+import com.isa.zuswebapp.freemarker.TemplateSupplier;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
+import javax.ejb.EJB;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@WebServlet("index")
+@WebServlet("")
 public class IndexServlet extends HttpServlet {
 
     Logger logger = Logger.getLogger(getClass().getName());
@@ -16,10 +27,34 @@ public class IndexServlet extends HttpServlet {
     Template template;
 
     @Override
-    public void init()throws ServletException{
+    public void init()throws ServletException {
         try {
-            template = TemplateProvider.create
+            template = TemplateSupplier.createTemplate(getServletContext(), "index.ftlh");
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
         }
 
     }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter pw= response.getWriter();
+        Map<String, Object> data  = new HashMap<>();
+
+        try{
+            template.process(data, pw);
+        }catch(TemplateException ex){
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+
 }
+
