af22b44b698235414ad37aa4cc9728db1a3f706d
Slawek Paciorek <slawekpaciorek@gmail.com>
2018-06-16 13:55:24 +0200

    Add new feature in ZUS, starts work with servlets

--- ZUSApp/src/main/java/com/infoshareacademy/PartsDetails.java
+++ ZUSApp/src/main/java/com/infoshareacademy/PartsDetails.java
@@ -1,20 +1,79 @@
 package com.infoshareacademy;
 
-import javax.json.JsonArray;
-import javax.json.JsonObject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Optional;
+import java.net.URL;
+import java.util.*;
 
 class PartsDetails {
 
     private String id;
     private HashMap<String, String> details;
-    private Optional<ArrayList<HashMap<String, String>>> properties;
-    private Optional<ArrayList<String>> pdfURLFile;
-    private Optional<ArrayList<String>> jpgURLFile;
-    private Optional<ArrayList<String>> additionalInfo;
+    private ArrayList<HashMap<String, String>> properties;
+    private ArrayList<String> pdfURLFile;
+    private ArrayList<String> jpgURLFile;
+    private ArrayList<String> additionalInfo;
+
+    private ObjectMapper objmap = new ObjectMapper();
+    private URL url;
+    private JsonNode jsonNode;
+
+    public String getId(String link) throws IOException{
+        String id;
+        String searchLink = JSONLoader.MAINLINK + link;
+
+        url = new URL(searchLink);
+        jsonNode = objmap.readTree(url);
+
+        JsonNode idNode = jsonNode.findPath("data").findPath("part").findPath("id");
+
+        id = idNode.asText();
+
+        return id;
+    }
+    public HashMap<String, String>getDetails(String link) throws IOException{
+        HashMap<String, String> details;
+        String searchLink = JSONLoader.MAINLINK + link;
+        url = new URL(searchLink);
+        jsonNode = objmap.readTree(url);
+
+        JsonNode detailNode = jsonNode.findPath("data").findPath("part").findPath("data");
+
+        details = objmap.convertValue(detailNode, HashMap.class);
+
+        return details;
+    }
+    public ArrayList<HashMap<String, String>> getProperties(String link) throws IOException{
+        ArrayList<HashMap<String,String>> properties = new ArrayList<>();
+        String searchLink = JSONLoader.MAINLINK + link;
+        url = new URL(searchLink);
+        jsonNode = objmap.readTree(url);
+        JsonNode propNode = jsonNode.findPath("data").findPath("part").get("properties");
+
+        for (int i = 0; i <propNode.size() ; i++) {
+            properties.add(objmap.convertValue(propNode.path(i), HashMap.class));
+        }
+
+        return properties;
+    }
+    public ArrayList<String> getPartData(String link, String dataName) throws IOException{
+        ArrayList<String> dataList;
+        String searchLink = JSONLoader.MAINLINK + link;
+        url = new URL(searchLink);
+        jsonNode = objmap.readTree(url);
+        JsonNode dataNode = jsonNode.findPath("data").findPath("part").findPath(dataName);
+
+        dataList = objmap.convertValue(dataNode, ArrayList.class);
+
+        return dataList;
+    }
+
 
 
 }
+
+
+
+
